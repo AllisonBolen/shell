8455d7f346525820684075583c7145e1087b0cef
Alec Allain <allainal@eos22.cis.gvsu.edu>
2018-09-09 18:29:01 -0400

    Added base/skeleton code to begin

---
+++ shell.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+* This class simulaltes a commmand line (shell)
+*
+* @author Allison Bolen
+* @author Alec Allain
+* @version 9/9/18
+*/
+
+/** Global variables */
+//char exit;
+char entry[81];
+int pid;
+int status;
+
+/**
+* Main method
+*/
+int main (int argc, char** argv) {
+    // instanciates variables
+    //exit = "quit"
+    pid = 0;
+    status = 0;
+
+    while (1) {
+        printf("\nShell: ");
+        scanf("%s80\n", entry);
+        if (strcmp(entry, "quit") == 0) {
+            break;
+        }
+        for (int i = 0; i < 81; i++) {
+
+        }
+        pid = fork();
+        if (pid < 0) {
+            fprintf(stderr, "The fork failed to produce a child");
+        }
+        else if (pid) {
+            waitpid(-1, &status, 0);
+        }
+        else {
+            //execve();
+        }
+    }
+    return 0;
+}
+
+void command () {
+
+}
